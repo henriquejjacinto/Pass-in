7bc4079c385630f975e979c51ba958c8864c9dd6
henriquejjacinto <hjacinto@ciant.com>
2026-06-02 18:18:18 -0300

    feat: initial project setup with database configuration

---
+++ src/main/java/henrique/com/pass_in/PassInApplication.java
@@ -0,0 +1,13 @@
+package henrique.com.pass_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInApplication.class, args);
+	}
+
+}
